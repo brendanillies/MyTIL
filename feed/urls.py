47923d47966e1534672027b182ed43d0c8db8752
Brendan Illies <brendan.illies@gmail.com>
2023-11-08 07:20:21 -0800

    Add PostDetailView

--- feed/urls.py
+++ feed/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'feed'
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='index')
+    path('', views.HomePageView.as_view(), name='index'),
+    path('<int:pk>/', views.PostDetailView.as_view(), name='detail')
 ]
