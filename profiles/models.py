8e1ad4b285905d4df3df2988dd9517d334d2a4e2
Brendan Illies <brendan.illies@gmail.com>
2023-11-08 06:58:02 -0800

    Add User -> Profile signal

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -8,8 +10,16 @@ class Profile(models.Model):
         on_delete=models.CASCADE,
         related_name='profile',
     )
-    
+
     def __str__(self):
         return self.user.username
 
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """
+    Create a new profile object when Django user is created
+    """
+
+    if created:
+        Profile.objects.create(user=instance)
